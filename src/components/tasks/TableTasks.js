4345659049da24e5b815b94f84e06c9136926487
dmitriymnv <minaevdup@gmail.com>
2018-12-24 15:02:19 +0300

    fix(components): выполнена новая вёрстка TableTasks
    В связи с тем что таблица из библиотеки @rmwc было сложно изменять, была сделана вёрстка новой
    таблицы, весь функционал был перенесён

--- src/components/tasks/TableTasks.js
+++ src/components/tasks/TableTasks.js
@@ -34,7 +34,7 @@ class TableTasks extends Component {
 	onScrollList = (e) => {
 		const { scrollTop, offsetHeight, scrollHeight } = e.target;
 		
-		const isCilentAtBottom = 30 +  scrollTop + 
+		const isCilentAtBottom = scrollTop + 
 		offsetHeight == scrollHeight;
 
     if (isCilentAtBottom) {
@@ -47,49 +47,92 @@ class TableTasks extends Component {
 	render() {
 		const { tasks, successTask, dialogAddTaskOpen } = this.props;
 		return (
-			<DataTable className="task-tables" onScroll={e => this.onScrollList(e)}>
-			{tasks.length === 0 ?
-				<TaskAdding dialogAddTaskOpen={dialogAddTaskOpen} /> :
-				<DataTableContent>
-					<DataTableHead >
-						<DataTableRow>
-							<DataTableHeadCell>Выполнение</DataTableHeadCell>
-							<DataTableHeadCell>Задача</DataTableHeadCell>
-							<DataTableHeadCell>Дата создания</DataTableHeadCell>
-							<DataTableHeadCell>Дата выполнения</DataTableHeadCell>
-						</DataTableRow>
-					</DataTableHead>
-					<DataTableBody>
-						{tasks.map((task, i) => {
-							return (
-								<DataTableRow
-									key={i}
-									selected={!!task.success}
-									className={`task-tables__row ${task.color}`}
-								>
-									<DataTableCell className="checkbox-task">
-										<Checkbox
-											checked={task.success}
-											onChange={() => {
-												successTask(task._id);
-											}}
-										/>
-									</DataTableCell>
-									<DataTableCell className="task-tables__title">{task.title}</DataTableCell>
-									<DataTableCell>
-										{new Date(task.dateCreate).toLocaleString('ru', options)}</DataTableCell>
-									<DataTableCell>
-										{task.dateCompletion ? 
-											new Date(task.dateCompletion).toLocaleString('ru', options): '-'
-										}
-								</DataTableCell>
-								</DataTableRow>
-							)}
-						)}
-					</DataTableBody>
-				</DataTableContent>
+			<>
+				{tasks.length == 0 ?
+					<TaskAdding dialogAddTaskOpen={dialogAddTaskOpen} /> :
+					<table className="task-tables">
+						<thead>
+							<tr>
+								<th>Выполнение</th>
+								<th className="task-tables__title">Задача</th>
+								<th>Дата создания</th>
+								<th>Дата выполнения</th>
+							</tr>
+						</thead>
+						<tbody onScroll={e => this.onScrollList(e)}>
+							{tasks.map((task, i) => {
+								return (
+									<tr
+										key={i}
+										className={`${task.success? 'success ' : ''}${task.color}`}
+									>
+										<td>
+											<Checkbox
+												checked={task.success}
+												onChange={() => {
+													successTask(task._id);
+												}}
+											/>
+										</td>
+										<td className="task-tables__title">{task.title}</td>
+										<td>
+											{new Date(task.dateCreate).toLocaleString('ru', options)}
+										</td>
+										<td>
+											{task.dateCompletion ? 
+												new Date(task.dateCompletion).toLocaleString('ru', options) : '-'
+											}
+										</td>
+									</tr>
+								)
+							})}
+						</tbody>
+					</table>
 				}
-		</DataTable>
+			</>
+		// 	<DataTable className="task-tables" onScroll={e => this.onScrollList(e)}>
+		// 	{tasks.length === 0 ?
+		// 		<TaskAdding dialogAddTaskOpen={dialogAddTaskOpen} /> :
+		// 		<DataTableContent>
+		// 			<DataTableHead >
+		// 				<DataTableRow>
+		// 					<DataTableHeadCell>Выполнение</DataTableHeadCell>
+		// 					<DataTableHeadCell>Задача</DataTableHeadCell>
+		// 					<DataTableHeadCell>Дата создания</DataTableHeadCell>
+		// 					<DataTableHeadCell>Дата выполнения</DataTableHeadCell>
+		// 				</DataTableRow>
+		// 			</DataTableHead>
+		// 			<DataTableBody>
+		// 				{tasks.map((task, i) => {
+		// 					return (
+		// 						<DataTableRow
+		// 							key={i}
+		// 							selected={!!task.success}
+		// 							className={`task-tables__row ${task.color}`}
+		// 						>
+		// 							<DataTableCell className="checkbox-task">
+		// 								<Checkbox
+		// 									checked={task.success}
+		// 									onChange={() => {
+		// 										successTask(task._id);
+		// 									}}
+		// 								/>
+		// 							</DataTableCell>
+		// 							<DataTableCell className="task-tables__title">{task.title}</DataTableCell>
+		// 							<DataTableCell>
+		// 								{new Date(task.dateCreate).toLocaleString('ru', options)}</DataTableCell>
+		// 							<DataTableCell>
+		// 								{task.dateCompletion ? 
+		// 									new Date(task.dateCompletion).toLocaleString('ru', options): '-'
+		// 								}
+		// 						</DataTableCell>
+		// 						</DataTableRow>
+		// 					)}
+		// 				)}
+		// 			</DataTableBody>
+		// 		</DataTableContent>
+		// 		}
+		// </DataTable>
 		)
 	}
 }
